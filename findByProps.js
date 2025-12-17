0581e1c546c8dadfea318fbafc20ddf9e4a4eb06
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2025-12-17 17:00:50 +0100

    update findbyprops

--- findByProps.js
+++ findByProps.js
@@ -1,16 +1,14 @@
-let wreq; webpackChunkdiscord_app.push([[Symbol()],{},(r) => wreq = r]);
+let _mods = webpackChunkdiscord_app.push([[Symbol()],{},r=>r.c]);
 webpackChunkdiscord_app.pop();
 
-let _mods = Object.values(wreq.c);
-
-const findByProps = (...props) => {
-    for (let m of _mods) {
+let findByProps = (...props) => {
+    for (let m of Object.values(_mods)) {
         try {
             if (!m.exports || m.exports === window) continue;
             if (props.every((x) => m.exports?.[x])) return m.exports;
 
             for (let ex in m.exports) {
-                if (props.every((x) => m.exports?.[ex]?.[x])) return m.exports[ex];
+                if (props.every((x) => m.exports?.[ex]?.[x]) && m.exports[ex][Symbol.toStringTag] !== 'IntlMessagesProxy') return m.exports[ex];
             }
         } catch {}
     }
